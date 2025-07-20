c7d6c7edbe84cb9f058e68aee7c7ac23978f83b5
Ashutosh mishra <mahendramishraabhi@gmail.com>
2025-07-20 14:39:12 +0530

    Add_details_log1

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-// ✅ Routes
-app.use('/api/auth', require('./Routes/auth'));
+
 
 // ✅ Middleware
 app.use(cors({
@@ -22,7 +21,8 @@ mongoose.connect(process.env.MONGO_URI, {
 
 }).then(() => console.log("MongoDB Connected..."))
   .catch(err => console.error("MongoDB Connection Error:", err));
-
+// ✅ Routes
+app.use('/api/auth', require('./Routes/auth'));
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
